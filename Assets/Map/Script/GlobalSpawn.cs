d78f8fd4d829b118e7a9e4f15d9b79d8597a97ca
Gregoire Picquette <gregoire.picquette@gmail.com>
2013-08-16 16:01:20 +0200

    Annimation + refonte lvl + number player sever

--- Assets/Map/Script/GlobalSpawn.cs
+++ Assets/Map/Script/GlobalSpawn.cs
@@ -4,20 +4,26 @@ using System.Collections;
 public class GlobalSpawn : MonoBehaviour {
 	public Transform playerPrefab;
     public Transform hudPrefab;
+	private bool GameLaunch = false;
+	private bool CanInitPlayer = false;
     // Use this for initialization
 	public void GameBegin() {
-        Transform playe = Network.Instantiate(playerPrefab, transform.position, transform.rotation, 0) as Transform;
-        Instantiate(hudPrefab); 
-        GUIManager hud = Object.FindObjectOfType(typeof(GUIManager)) as GUIManager;
-        hud.player = playe.gameObject;
+		CanInitPlayer = true;
+		//Camera.mainCamera.animation.Play("levelAnnimatio");
     }
 	
 	void Start () {
-	
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (!Camera.mainCamera.animation.isPlaying && !GameLaunch && CanInitPlayer) {
+			Transform playe = Network.Instantiate(playerPrefab, transform.position, transform.rotation, 0) as Transform;
+        	Instantiate(hudPrefab); 
+        	GUIManager hud = Object.FindObjectOfType(typeof(GUIManager)) as GUIManager;
+        	hud.player = playe.gameObject;
+			GameLaunch = true;
+		}
 	}
 }
